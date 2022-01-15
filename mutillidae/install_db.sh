48c023183ed66ac4c5d7ee253804aa69a8166857
Carsten Lucke <github@lucke.info>
2022-01-15 01:11:40 +0100

    Update install_db.sh

--- mutillidae/install_db.sh
+++ mutillidae/install_db.sh
@@ -11,11 +11,8 @@ if [ -f /root/.my.cnf ]; then
 
 # If /root/.my.cnf doesn't exist then it'll ask for root password   
 else
-    echo "Please enter root user MySQL password!"
-    echo "Note: password will be hidden when typing"
-    read -sp rootpasswd
-    mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
-    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
-    mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+    mysql -uroot -p"" -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+    mysql -uroot -p"" -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+    mysql -uroot -p"" -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+    mysql -uroot -p"" -e "FLUSH PRIVILEGES;"
 fi
