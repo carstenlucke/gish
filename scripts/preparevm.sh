89a15164cd8258a5584b39be1c6d2df39aef230c
Carsten Lucke <github@lucke.info>
2022-01-16 21:44:02 +0100

    Update preparevm.sh

--- scripts/preparevm.sh
+++ scripts/preparevm.sh
@@ -5,6 +5,7 @@ sudo apt-get install php-curl php-mbstring php-xml
 sudo apt-get upgrade
 # sudo apt-get --with-new-pkgs upgrade
 echo "+ + + Installation der Datenbank für mutillidae + + +"
+sudo /etc/init.d/mariadb start
 echo "DB-root Kennwort eingeben (einfach ENTER, wenn nicht anders gesetzt!"
 sudo mysql -uroot -p -e "CREATE DATABASE mutillidae /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 git clone https://github.com/carstenlucke/gish.git
@@ -12,4 +13,3 @@ cd gish
 sudo mv /var/www/html /var/www/html-BACKUP
 sudo mv var_www_html /var/www/html
 sudo /etc/init.d/apache2 start
-sudo /etc/init.d/mariadb start
