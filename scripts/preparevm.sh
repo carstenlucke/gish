231c2cf46234b73b8e5336e1aded2dd9a3eb3ef5
Carsten Lucke <github@lucke.info>
2022-01-17 00:08:38 +0100

    Update preparevm.sh

--- scripts/preparevm.sh
+++ scripts/preparevm.sh
@@ -14,15 +14,20 @@ echo "DB-root Kennwort eingeben (einfach ENTER, wenn nicht anders gesetzt!"
 sudo mysql -uroot -p"kali" -e "CREATE DATABASE mutillidae /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 
 # php settings must be insecure
+PHPINI="/etc/php/7.4/apache2/php.ini"
+
 allow_url_include=On
 allow_url_fopen=On
+
 for key in allow_url_include allow_url_fopen
 do
- sed -i "s/^\($key\).*/\1 $(eval echo = \${$key})/" /etc/php/7.4/apache2/php.ini
+ sudo sed -i "s/^\($key\).*/\1 $(eval echo = \${$key})/" $PHPINI
 done
 echo "+ + + Checking PHP setting changes + + +"
-cat /etc/php/7.4/apache2/php.ini | grep allow_url_
+cat $PHPINI | grep allow_url_
 
+
+# Checkout files from github repo
 git clone https://github.com/carstenlucke/gish.git
 cd gish
 sudo mv /var/www/html /var/www/html-BACKUP
