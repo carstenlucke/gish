38bdd286b60511ba04705699a3c00bc4082a2066
Carsten Lucke <github@lucke.info>
2022-01-16 19:57:57 +0100

    Update preparevm.sh

--- scripts/preparevm.sh
+++ scripts/preparevm.sh
@@ -3,6 +3,8 @@ cd ~
 sudo apt-get update
 sudo apt-get install php-curl php-mbstring php-xml
 sudo apt-get --with-new-pkgs upgrade
+echo "+ + + Installation der Datenbank für mutillidae + + +"
+echo "DB-root Kennwort eingeben (einfach ENTER, wenn nicht anders gesetzt!"
 sudo mysql -uroot -p -e "CREATE DATABASE mutillidae /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 git clone https://github.com/carstenlucke/gish.git
 cd gish
