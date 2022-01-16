efc3203fef9dbd6529822f79a37613c53d3e8e0c
Carsten Lucke <github@lucke.info>
2022-01-16 19:48:52 +0100

    Update preparevm.sh

--- scripts/preparevm.sh
+++ scripts/preparevm.sh
@@ -5,5 +5,8 @@ sudo apt-get install php-curl php-mbstring php-xml
 sudo apt-get --with-new-pkgs upgrade
 sudo mysql -uroot -p -e "CREATE DATABASE mutillidae /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 git clone https://github.com/carstenlucke/gish.git
+cd gish
+sudo mv /var/www/html /var/www/html-BACKUP
+sudo mv var_www_html /var/www/
 sudo /etc/init.d/apache2 start
 sudo /etc/init.d/mariadb start
